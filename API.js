eddd12a4a4a164847f848fa2bd486cb816f28744
ru55-2 <rm_leavitt@icloud.com>
2023-09-22 14:00:15 -0600

    first git update

---
+++ API.js
@@ -0,0 +1,77 @@
+import mysql from 'mysql2';
+import fs from 'fs';
+import dotenv from 'dotenv';
+dotenv.config()
+
+//connection to the server:
+const pool = mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  ssl: {
+    ca: fs.readFileSync('certificate.crt'), 
+  }
+}).promise();
+
+
+//function to get all of the stuff in the database
+export async function getdatabase() {
+  const sql = 'SELECT * FROM volleyball.Matches';
+
+  const [res] = await pool.query(sql);
+
+
+
+  return res;
+  pool.end()
+}
+
+
+
+// Function to create a game
+export async function creategame(Time, Team1, Team2, Location) {
+  const sql = 'INSERT INTO volleyball.Matches (MatchTime, Team1, Team2, Location) VALUES (?, ?, ?, ?)';
+
+  const [res] = await pool.query(sql, [Time, Team1, Team2, Location]);
+
+  return 'game added';
+  pool.end()
+}
+
+
+
+// Function to update a set
+export async function updateset(id, SET_NUM, T1S_update, T2S_update) {
+  const ST1 = `S${SET_NUM}T1`;
+  const ST2 = `S${SET_NUM}T2`;
+
+  const sql = `UPDATE volleyball.Matches SET \`${ST1}\` = ?, \`${ST2}\` = ? WHERE MatchID = ?`;
+  
+  await pool.query(sql, [T1S_update, T2S_update, id]);
+
+  
+  return 'updated';
+  pool.end()
+}
+
+
+
+
+
+
+
+
+
+
+//syntax for creating a server query.
+
+/*
+const update = await updateset('3','3','5','12');
+console.log(update)
+
+
+const Create = await creategame('2023-09-22 9:00:00', 'LCI','CCH','LCI small gym')
+console.log(Create)
+
+*/
---
+++ APP.js
@@ -0,0 +1,69 @@
+import express from 'express'
+const app = express();
+app.use(express.json());
+
+import { creategame, updateset, getdatabase } from './API.js'
+
+
+
+
+//gets all of the games in the data base
+app.get("/games", async (req,res) => {
+    const games = await getdatabase();
+    res.send(games)
+})
+
+
+//create a game
+app.post("/creategame",async (req,res) =>{
+    const time = req.body.time;
+    const team1 = req.body.team1;
+    const team2 = req.body.team2;
+    const location = req.body.location;
+    const ask = await creategame(time,team1,team2,location)
+    res.send(ask)
+});
+
+
+
+//updates the selected set
+app.put("/updategame", async (req,res)=>{
+    const id = req.body.id;
+    const setnum = req.body.setnum;
+    const t1su = req.body.t1su;
+    const t2su = req.body.t2su;
+    const ask = await updateset(id,setnum,t1su,t2su);
+    res.send(ask);
+
+});
+
+/*
+{
+  "id":11,
+  "setnum":3,
+  "t1su":20,
+  "t2su":25
+}
+{
+  "time":"2022-09-22 9:00",
+  "team1":"LCI",
+  "team2":"CCH",
+  "location":"LCI large gym"
+  
+}
+*/
+
+
+
+
+
+app.use((err,req,res, next) => {
+    console.error(err.stack);
+    res.status(500).send('something broke!');
+});
+
+
+app.listen(8080, () => {
+console.log('server is running on port 8080');
+});
+
