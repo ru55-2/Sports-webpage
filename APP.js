3d7235bf3d02e8ed281203c505465734c33ceb23
ru55-2 <rm_leavitt@icloud.com>
2023-09-24 16:28:53 -0600

    changes the the app to help the browser in rendering

--- APP.js
+++ APP.js
@@ -5,7 +5,8 @@ app.use(express.json());
 //fixing cors problems
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
-  // Add other necessary headers (e.g., methods, headers) if needed.
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
 
