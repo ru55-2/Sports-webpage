166ec07cd87f705ac295081e931057fb25c2e3eb
ru55-2 <rm_leavitt@icloud.com>
2023-09-24 20:57:58 -0600

    changing some settings to let the mobile version work

--- APP.js
+++ APP.js
@@ -5,11 +5,13 @@ app.use(express.json());
 //fixing cors problems
 app.use((req, res, next) => {
   // Combine both allowed origins into a single comma-separated string
-  res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+  const allowedOrigins = 'http://127.0.0.1:5500, http://10.0.0.177:5500';
+  res.header('Access-Control-Allow-Origin', allowedOrigins);
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
+
 
 
 
