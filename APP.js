7bd14e87178f230eec98913d1a9b52a65e359095
ru55-2 <rm_leavitt@icloud.com>
2023-09-25 17:27:00 -0600

    adding single game stats functions

--- APP.js
+++ APP.js
@@ -4,7 +4,7 @@ app.use(express.json());
 
 //fixing cors problems
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
