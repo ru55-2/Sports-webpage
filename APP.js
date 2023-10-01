6fef0badbe325002c91382fa0d7cb030a68bd306
ru55-2 <rm_leavitt@icloud.com>
2023-10-01 12:44:50 -0600

    updating the CORS access

--- APP.js
+++ APP.js
@@ -4,7 +4,7 @@ app.use(express.json());
 
 //fixing cors problems
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://sports.leavittservers.xyz');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
