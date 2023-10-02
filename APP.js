5a905ed873a894996c1df4f92fa5f29a00f4fe5a
ru55-2 <rm_leavitt@icloud.com>
2023-10-02 12:42:09 -0600

    this will maybe work????

--- APP.js
+++ APP.js
@@ -8,6 +8,12 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    return res.status(200).end();
+  }  
 });
 
 
