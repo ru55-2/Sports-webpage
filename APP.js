118e7fcd20c0cae0a46d4388963f5fc5e32d5a29
ru55-2 <rm_leavitt@icloud.com>
2023-10-02 13:02:00 -0600

    removig the cors entierly to see what happens

--- APP.js
+++ APP.js
@@ -2,19 +2,7 @@ import express from 'express'
 const app = express();
 app.use(express.json());
 //a change
-//fixing cors problems
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-  if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    return res.status(200).end();
-  }  
-});
+
 
 
 
