c33a72b3bc1f03e887cb2bbe5649aa80d3889c44
ru55-2 <rm_leavitt@icloud.com>
2023-10-20 21:06:15 -0600

    changing naming in the api

--- APP.js
+++ APP.js
@@ -7,7 +7,7 @@ app.use(express.json());
 import { creategame, updateset, getdatabase, singlegameinfo, deletegame, checkadmin} from './API.js'
 
 //delete a game
-app.delete("/api/delete", async (req,res) => {
+app.delete("/api/volleyball/delete", async (req,res) => {
   const gameid = req.query.id;
   const ask = await deletegame(gameid);
   res.send(ask)
@@ -17,10 +17,12 @@ app.delete("/api/delete", async (req,res) => {
 
 
 //gets all of the games in the data base
-app.get("/api/games", async (req,res) => {
+app.get("/api/volleyball/games", async (req,res) => {
     const games = await getdatabase();
     res.send(games)
 })
+
+//returns a true or false if the admin is in the database or not
 app.post("/api/checkadmin",async (req,res) => {
   const pass = req.body.pass;
   const usr = req.body.usr;
@@ -29,7 +31,7 @@ app.post("/api/checkadmin",async (req,res) => {
 })
 
 //create a game
-app.post("/api/creategame",async (req,res) =>{
+app.post("/api/volleyball/creategame",async (req,res) =>{
     const time = req.body.time;
     const team1 = req.body.team1;
     const team2 = req.body.team2;
@@ -39,7 +41,7 @@ app.post("/api/creategame",async (req,res) =>{
     res.send(ask)
 });
 //get stats for a game
-app.post("/api/getgame",async (req,res) => {
+app.post("/api/volleyball/getgame",async (req,res) => {
   const matchid = req.body.id;
   const ask = await singlegameinfo(matchid)
   res.send(ask)
@@ -47,7 +49,7 @@ app.post("/api/getgame",async (req,res) => {
 
 
 //updates the selected set
-app.put("/api/updategame", async (req,res)=>{
+app.put("/api/volleyball/updategame", async (req,res)=>{
     const id = req.body.id;
     const setnum = req.body.setnum;
     const t1su = req.body.t1su;
